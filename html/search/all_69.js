6a005a975479b50c77062037938800077b5420ce
Travis CI <travis@travis-ci.org>
2016-12-16 18:51:15 +0000

    Deploy code docs to GitHub Pages Travis build: 17
    Commit: 6d3a45628eb9588dd3b6b756ba0f977eb8b883ef

---
+++ html/search/all_69.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['inicializarcalculador',['inicializarCalculador',['../class_l_dy_h_1_1_j_unit_levenshtein_1_1_app_test.html#a31cef9bb44ad262367cbf2d691ab4865',1,'LDyH::JUnitLevenshtein::AppTest']]]
+];
---
+++ html/search/functions_63.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['cadenasdiferentes',['cadenasDiferentes',['../class_l_dy_h_1_1_j_unit_levenshtein_1_1_app_test.html#aecd16fe611b3c00305747520c084c3ef',1,'LDyH::JUnitLevenshtein::AppTest']]],
+  ['cadenasequivalentes',['cadenasEquivalentes',['../class_l_dy_h_1_1_j_unit_levenshtein_1_1_app_test.html#a8d7ed8387a543266a6031358f1100a8b',1,'LDyH::JUnitLevenshtein::AppTest']]],
+  ['cerrarcalculador',['cerrarCalculador',['../class_l_dy_h_1_1_j_unit_levenshtein_1_1_app_test.html#a7fc09416a649d1fe1f293e941ad079c3',1,'LDyH::JUnitLevenshtein::AppTest']]]
+];
